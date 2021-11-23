ba325e44797001f983a99f03524323de5a4a6516
Janne Tuukkanen <jatufin@gmail.com>
2021-11-23 22:38:06 +0200

    Testikattavuudesta taski

--- tasks.py
+++ tasks.py
@@ -7,3 +7,12 @@ def start(ctx):
 @task
 def test(ctx):
     ctx.run("pytest src")
+
+@task
+def coverage(ctx):
+    ctx.run("coverage run --branch -m pytest src")
+
+@task(coverage)
+def coverage_report(ctx):
+    ctx.run("coverage html")
+
