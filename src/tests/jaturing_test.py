e0da513de2252eff0193c7f437cdfc6ce759b28c
Janne Tuukkanen <jatufin@gmail.com>
2021-12-07 23:45:39 +0200

    Created more tests

--- src/tests/jaturing_test.py
+++ src/tests/jaturing_test.py
@@ -39,3 +39,36 @@ class TestJaturing(unittest.TestCase):
                                next_state="q2")
         self.assertEqual(str(self.jaturing.get_state("q1").rules["a"]),
                          "A;RIGHT;q2")
+
+    def test_tape_returns_tape(self):
+        self.assertIsNotNone(self.jaturing.tape)
+
+    def test_delete_nonexisting_state_deletes_nothing(self):
+        self.jaturing.set_rule(state_name="q1",
+                               character="a",
+                               write_char="A",
+                               direction="RIGHT",
+                               next_state="q2")
+        before = str(self.jaturing)
+        self.jaturing.delete_state('q2')
+        after = str(self.jaturing)
+        self.assertEqual(before, after)
+
+    def test_deleting_state_deletes_state(self):
+        self.jaturing.set_rule(state_name="q1",
+                               character="a",
+                               write_char="A",
+                               direction="RIGHT",
+                               next_state="q2")
+        self.jaturing.delete_state('q1')
+        self.assertTrue("q1" not in self.jaturing._states)
+        
+    def test_deleting_current_state_sets_current_state_to_none(self):
+        self.jaturing.set_rule(state_name="q1",
+                               character="a",
+                               write_char="A",
+                               direction="RIGHT",
+                               next_state="q2")
+        self.jaturing.current_state = "q1"
+        self.jaturing.delete_state('q1')
+        self.assertEqual(self.jaturing.current_state, None)
--- src/tests/state_test.py
+++ src/tests/state_test.py
@@ -20,3 +20,19 @@ class TestState(unittest.TestCase):
         self.state.set_rule("b", "q1", "RIGHT", "A")
         self.assertEqual(len(self.state.rules), 2)
 
+    def test_nonexisting_character_returns_no_rule(self):
+        self.assertEqual(self.state.get_rule('a'), None)
+
+    def test_deleting_nonexisting_rule_returns_nothing(self):
+        self.assertEqual(self.state.delete_rule('a'), None)
+
+    def test_deleting_rule_deletes_rule(self):
+        self.state.set_rule("a", "q1", "RIGHT", "B")
+        self.state.delete_rule("a")
+        rule = self.state.get_rule("a")
+        self.assertEqual(str(rule), 'None')
+
+    def test_get_rules_in_dictionary_returns_dictionary(self):
+        self.state.set_rule("a", "q1", "RIGHT", "B")
+        dict = self.state.get_rules_in_dictionary()
+        self.assertEqual(dict, {'a': {'direction': 'RIGHT', 'next_state': 'B', 'write_char': 'q1'}})
